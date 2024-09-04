8aba9c43e3234e0fc9cd363e8589249e1af69804
Pushpasri M <127647687+pushpasri-M@users.noreply.github.com>
2024-09-04 15:34:43 -0400

    Add second commit

---
+++ C2TC_sri/src/com/tns/day2/LoopStatement.java
@@ -0,0 +1,27 @@
+package com.tns.day2;
+
+public class LoopStatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  int i=0;
+		  int r=0;
+	      while (i<=10)
+	      {
+	        System.out.println(i);
+	        i++;
+	      }
+	      
+	      for (r=0;r<=10;r++)
+	       {
+	         System.out.println(r);
+	       }
+	      do
+	      {
+	        System.out.println(i);
+	        i++;
+	      }while(i<=10);
+
+	}
+ 
+}
