d60c60dcef2fdfdff2fa23818b8e8dbd49380fe0
Pushpasri M <127647687+pushpasri-M@users.noreply.github.com>
2024-09-06 11:33:37 -0400

    Add commit

---
+++ C2TC_pushpasri/src/com/tns/day2/IfDemo.java
@@ -0,0 +1,10 @@
+package com.tns.day2;
+
+public class IfDemo {
+	public static void main(String[] args) { 
+		 
+		  int x = 9; 
+		  if (x != 5) 
+		   System.out.println("Value of x is not 5"); 
+		 }
+}
